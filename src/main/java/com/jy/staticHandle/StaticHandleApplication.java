3613ec22be75a7f666f7970a2c9af1ddad0d1670
Juyoung Kim <kimmykim.jy@gmail.com>
2022-06-30 11:21:27 +0900

    new project

---
+++ src/main/java/com/jy/staticHandle/StaticHandleApplication.java
@@ -0,0 +1,13 @@
+package com.jy.staticHandle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticHandleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaticHandleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jy/staticHandle/StaticHandleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jy.staticHandle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticHandleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
